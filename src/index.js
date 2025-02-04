650eff9bd860cdfead94e352ffc567586238fd78
Frida Anselin <frida.anselin@se.experis.com>
2025-02-04 14:19:22 +0100

    core complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 
-console.log(data);
+const cardsUL = document.querySelector(".cards");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function renderCards() {
+    cardsUL.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        const card = data[i]
+        const cardLi = document.createElement('li')
+        cardLi.setAttribute('class', 'card')
+        const cardTitle = document.createElement('h2')
+        cardTitle.setAttribute('class', 'card--title')
+        const cardImg = document.createElement('img')
+        cardImg.setAttribute('class','card--img')
+        const cardText = document.createElement('ul')
+        cardText.setAttribute('class', 'card--text')
+
+        cardTitle.textContent = card.name;
+        cardImg.src = card.sprites.front_default;
+        cardImg.width = 256;
+        cardLi.appendChild(cardTitle);
+        cardLi.appendChild(cardImg);
+
+        for (let i = 0; i < card.stats.length; i++) {
+            const pokemonStats = document.createElement('li');
+            pokemonStats.textContent = card.stats[i].stat.name + ": " + card.stats[i].base_stat
+            cardText.appendChild(pokemonStats);
+        }
+        cardLi.appendChild(cardText)
+        cardsUL.appendChild(cardLi);
+    }
+}
+
+function main() {
+    renderCards()
+}
+
+main()
